d31e0fda84b7b7c57be908bc2a28c897b345e2c1
Test2024 <Test2024@192.168.1.41>
2025-04-02 18:46:11 +0200

    app

--- modorder25/src/main/java/com/srsoft/modorder25/Modorder25Application.java
+++ modorder25/src/main/java/com/srsoft/modorder25/Modorder25Application.java
@@ -2,8 +2,12 @@ package com.srsoft.modorder25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.srsoft.modaccessk01.entity")
+@EnableJpaRepositories(basePackages = {"com.srsoft.modaccessk01.repository"})
 public class Modorder25Application {
 
 	public static void main(String[] args) {
